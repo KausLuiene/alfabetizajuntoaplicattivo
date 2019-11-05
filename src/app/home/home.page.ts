ad722ab24cd86f148362992c5db9406b8ebae9f8
KausLuiene <54158165+KausLuiene@users.noreply.github.com>
2019-11-05 12:42:32 -0300

    aplicativoalfabetizajunto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  // @ViewChild('Slides', {static: false}) slides: IonSlides;
+
+  url = 'https://spreadsheets.google.com/feeds/list/1wlq5rZEfXLtSDsAEvPAur9Oos0zBK3MVm3fODGppQUA/od6/public/values?alt=json';
+  frases: any =  [];
+  frase = '';
+  fonte = '';
 
   constructor() {}
 
+  ngOnInit() {
+    const options = { method: 'GET' };
+    fetch(this.url, options)
+      .then(response => {
+        return response.json();
+    })
+    .then(data => {
+      this.frases = data.feed.entry;
+      console.log(this.frases);
+      this.frase = this.frases[Math.floor((Date.now() / 86400000) % this.frases.length)].gsx$post.$t;
+      this.fonte = this.frases[Math.floor((Date.now() / 86400000) % this.frases.length)].gsx$fonte.$t;
+      // this.slides.slideTo(Math.floor((Date.now() / 86400) % this.frases.length));
+    });
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then(() => this.notificarFrase());
+    } else {
+      this.notificarFrase();
+    }
+  }
+notificarFrase() {
+   const notification = new Notification('AlfabetizaJunto tem uma nova informação para você')
+  }
 }
